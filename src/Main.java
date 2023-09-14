f763965af6ebc321d4886fbd6d568f267f52e41e
Hello world <Opokukka@mail.uc.edu>
2023-09-14 16:34:34 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double summerCost = 10;
+        double winterCost = 10;
+        double fallCost = 10;
+        double springCost = 10;
+        double totalCost = 0;
+
+        totalCost = summerCost + winterCost + fallCost + springCost;
+
+        System.out.println("The total yearly cost from all four seasons is " + totalCost);
+    }
+}
